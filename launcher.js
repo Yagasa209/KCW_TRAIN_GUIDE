8a23313eb9b70795c8307d06236a8c4458e146de
Yagasa209 <95123939+Yagasa209@users.noreply.github.com>
2021-11-27 22:53:44 +0900

    Update launcher.js

--- launcher.js
+++ launcher.js
@@ -3,7 +3,7 @@ if (scr_parent == null) {
   alert("[for管理者]\n設置する際に <div id=\"guide_main\"></div>　を置き忘れています。");
   throw "div error";
 }
-var scr_dataurl = (location.protocol == "file:") ? "./data.js" : "https://thenyutheta.github.io/KCW_TRAIN_GUIDE/data.js";
+var scr_dataurl = (location.protocol == "file:") ? "./data.js" : "https://Yagasa209.github.io/KCW_TRAIN_GUIDE/data.js";
 var scr_coreurl = (location.protocol == "file:") ? "./core.js" : "https://thenyutheta.github.io/KCW_TRAIN_GUIDE/core.js";
 var scr_data = document.createElement("script");
 scr_data.src = scr_dataurl + '?t=' + Date.now();
